f03f9f0efbb1dc34822c3480a437a846834ed04e
Josh Cunningham <josh.cu@gmail.com>
2025-04-18 13:18:04 -0500

    add run cli option

--- src/ngiab_cal/__main__.py
+++ src/ngiab_cal/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import sqlite3
+import subprocess
 import sys
 import warnings
 from datetime import datetime, timedelta
@@ -12,7 +13,7 @@ from aiohttp.client_exceptions import ContentTypeError
 from hydrotools.nwis_client import IVDataService
 
 from ngiab_cal.custom_logging import setup_logging
-from ngiab_cal.file_paths import FilePaths
+from ngiab_cal.file_paths import FilePaths, validate_input_folder
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
@@ -160,6 +161,7 @@ def create_calibration_config(data_folder: Path, gage_id: str) -> None:
         )
 
     start, end = get_start_end_times(files.template_realization)
+
     write_usgs_data_to_csv(start, end, gage_id, files.observed_discharge)
     create_crosswalk_json(files.geopackage_path, gage_id, files.crosswalk)
     # copy the ngen realization and troute config files into the calibration folder
@@ -174,6 +176,20 @@ def create_calibration_config(data_folder: Path, gage_id: str) -> None:
     logging.warning(f'docker run -it -v "{files.data_folder}:/ngen/ngen/data" joshcu/ngiab-cal')
 
 
+def run_calibration(folder_to_run: Path) -> None:
+    try:
+        subprocess.run("docker pull joshcu/ngiab-cal", shell=True)
+    except:
+        logging.error("Docker is not running, please start Docker and try again.")
+    logging.warning("Beginning calibration...")
+    try:
+        command = f'docker run --rm -it -v "{str(folder_to_run)}:/ngen/ngen/data" --user $(id -u):$(id -g) joshcu/ngiab-cal /calibration/run.sh'
+        subprocess.run(command, shell=True)
+        logging.info("Calibration complete.")
+    except:
+        logging.error("Calibration failed.")
+
+
 def main():
     setup_logging()
     parser = argparse.ArgumentParser(description="Create a calibration config for ngen-cal")
@@ -183,8 +199,24 @@ def main():
         help="Path to the folder you wish to calibrate",
     )
     parser.add_argument("-g", "--gage", type=str, help="Gage ID to use for calibration")
+    parser.add_argument(
+        "-f", "--force", help="Overwrite existing configuration", action="store_true"
+    )
+    parser.add_argument(
+        "--run",
+        help="Try to automatically run the calibration, this may be unstable",
+        action="store_true",
+    )
     args = parser.parse_args()
-    create_calibration_config(args.data_folder, args.gage)
+    paths = FilePaths(args.data_folder)
+    config_valid = validate_input_folder(paths, skip_calibration_folder=False)
+
+    if not config_valid or args.force:
+        create_calibration_config(args.data_folder, args.gage)
+
+    if args.run:
+        logging.info(f"Starting calibration run at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        run_calibration(args.data_folder)
 
 
 if __name__ == "__main__":
